c9b70423160e463b80644fe3c5004175a8a0c690
Matthew Beale <matt.beale@madhatted.com>
2013-09-20 15:47:44 -0400

    Introduce a didTransition event, fired on the entered route.
    didTransition mirrors willTransition

--- dist/router.js
+++ dist/router.js
@@ -806,7 +806,7 @@
     @private
 
     This function is called when transitioning from one URL to
-    another to determine which handlers are not longer active,
+    another to determine which handlers are no longer active,
     which handlers are newly active, and which handlers remain
     active but have their context changed.
 
@@ -1000,6 +1000,9 @@
             currentHandlerInfos.length !== matchPointResults.matchPoint) {
           finalizeTransition(transition, handlerInfos);
         }
+
+        // currentHandlerInfos was updated in finalizeTransition
+        trigger(router, router.currentHandlerInfos, true, ['didTransition']);
 
         if (router.didTransition) {
           router.didTransition(handlerInfos);
