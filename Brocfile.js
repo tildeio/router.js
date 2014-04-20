77af6f7a70610bac69509c091f10df2d24e1fa7b
pangratz <cmueller.418@gmail.com>
2014-04-21 01:36:33 +0200

    Eat your vegetables: Broccolification
    This commit replaces the Grunt tasks - which compile the ES6 router.js
    modules into CommonJS, AMD and standalone Browser versions - with Broccoli.
    
    A new build of router.js can now be created via `broccoli build dist`.
    Automatic re-compilation is available via `broccoli serve`, while the
    tests are executable via `http://localhost:4200/tests/index.html`.
    
    Running the tests via the command line is possible via `grunt test`.

---
+++ Brocfile.js
@@ -0,0 +1,136 @@
+var concat = require('broccoli-concat');
+var filterES6Modules = require('broccoli-es6-module-filter');
+var mergeTrees = require('broccoli-merge-trees');
+var moveFile = require('broccoli-file-mover');
+var pickFiles = require('broccoli-static-compiler');
+var uglifyJavaScript = require('broccoli-uglify-js');
+var wrapFiles = require('broccoli-wrap');
+
+var trees = [
+  createAMDTree(),
+  createCommonJSTree(),
+  createStandaloneTree(),
+
+  // TODO only add tests when Broccoli environment is development ...
+  makeTests()
+];
+
+module.exports = mergeTrees(trees);
+
+function makeTests() {
+  // Concatenate all dependencies into tests/deps.js
+  var deps = concat('vendor/deps', {
+    inputFiles: ['*.js'],
+    outputFile: '/tests/deps.js'
+  });
+
+  // Create AMD module 'tests' containing all tests in 'test/tests' and concatenate them into tests/tests.js
+  var tests = filterES6Modules('test/tests', {
+    moduleType: 'amd',
+    packageName: 'tests',
+    anonymous: false
+  });
+  tests = concat(tests, {
+    inputFiles: ['**/*.js'],
+    outputFile: '/tests/tests.js'
+  });
+
+  // Copy files needed for QUnit
+  var qunit = pickFiles('test', {
+    files:  ['index.html', 'vendor/*'],
+    srcDir: '/',
+    destDir: '/tests'
+  });
+
+  // Copy vendor/loader.js to test/loader.js
+  var loader = concat('vendor', {
+    inputFiles: ['loader.js'],
+    outputFile: '/tests/loader.js'
+  });
+
+  // Merge all test related stuff into tests tree
+  return mergeTrees([deps, qunit, loader, tests]);
+}
+
+
+
+function createAMDTree() {
+  // dist/router.amd.js: all AMD compiled modules concatenated into 1 file
+  var amd = filterES6Modules('lib', {
+    moduleType: 'amd',
+    anonymous: false
+  });
+
+  amd = concat(amd, {
+    // to be consinstent with old behavior, we include 'router.js' at the end
+    inputFiles: ['router/**/*.js', 'router.js'],
+    outputFile: '/router.amd.js'
+  });
+
+  return amd;
+}
+
+
+
+function createCommonJSTree() {
+  // CommonJS version of router.js; will be located in 'dist/commonjs'
+  var commonJs = pickFiles('lib', {
+    srcDir: '/',
+    destDir: '/commonjs'
+  });
+  commonJs = filterES6Modules(commonJs, {
+    moduleType: 'cjs'
+  });
+
+  // rename router.js to main.js
+  commonJs = moveFile(commonJs, {
+    srcFile: '/commonjs/router.js',
+    destFile: '/commonjs/main.js'
+  });
+
+  return commonJs;
+}
+
+
+
+function createStandaloneTree() {
+  // dist/router.js: IIFE version of router.js, using RSVP and RouteRecognizer globals
+  var begin = '(function(globals, RSVP, RouteRecognizer) {\n';
+  var end = [];
+  end.push('define("route-recognizer", [], function() { return {default: RouteRecognizer}; });');
+  end.push('define("rsvp", [], function() { return RSVP;});');
+  end.push('define("rsvp/promise", [], function() { return {default: RSVP.Promise}; });');
+  end.push("window.Router = requireModule('router');");
+  end.push('}(window, window.RSVP, window.RouteRecognizer));');
+  end = end.join('\n');
+
+  var browser = pickFiles('vendor', {
+    files: ['loader.js'],
+    srcDir: '/',
+    destDir: '/'
+  });
+  browser = mergeTrees([browser, createAMDTree()]);
+  browser = concat(browser, {
+    inputFiles: ['loader.js', '*.js'],
+    outputFile: '/router.js'
+  });
+  browser = wrapFiles(browser, {
+    wrapper: [begin, end],
+    extensions: ['js']
+  });
+
+  // dist/router.min.js
+  var minified = pickFiles(browser, {
+    srcDir: '/',
+    destDir: '/'
+  });
+  minified = moveFile(minified, {
+    srcFile: '/router.js',
+    destFile: '/router.min.js'
+  });
+  minified = uglifyJavaScript(minified, {
+    mangle: true
+  });
+
+  return mergeTrees([browser, minified]);
+}
--- Gruntfile.js
+++ Gruntfile.js
@@ -10,26 +10,19 @@ module.exports = function(grunt) {
 
   // Run client-side tests on the command line.
   this.registerTask('test', 'Runs tests through the command line using PhantomJS', [
-    'build', 'tests', 'qunit'
+    'build', 'qunit'
   ]);
 
   // Run a server. This is ideal for running the QUnit tests in the browser.
-  this.registerTask('server', ['build', 'tests', 'connect', 'watch:server']);
-
-
-  // Build test files
-  this.registerTask('tests', 'Builds the test package', ['concat:deps',
-                    'transpile:testsAmd', 'transpile:testsCommonjs', 'buildTests:dist']);
+  this.registerTask('server', ['broccoli:dist:serve']);
 
   // Build a new version of the library
   this.registerTask('build', 'Builds a distributable version of <%= cfg.name %>',
-                    ['clean', 'transpile:amd', 'transpile:commonjs', 'concat:amdNoVersion',
-                      'concat:browser', 'browser:distNoVersion', 'jshint', 'uglify:browserNoVersion']);
+                    ['clean', 'broccoli:dist:build', 'jshint']);
 
   config.env = process.env;
   config.pkg = grunt.file.readJSON('package.json');
+
   // Load custom tasks from NPM
-  grunt.loadNpmTasks('grunt-browserify');
-  // Merge config into emberConfig, overwriting existing settings
   grunt.initConfig(config);
 };
