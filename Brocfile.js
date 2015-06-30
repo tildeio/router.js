038b2538ca56b2e85e8ca5b07346b0448597463b
Kurt Ruppel <me@kurtruppel.com>
2015-06-30 16:52:39 -0700

    Create a UMD dist file and use as main entry point for module.

--- Brocfile.js
+++ Brocfile.js
@@ -1,5 +1,5 @@
 var concat = require('broccoli-concat');
-var filterES6Modules = require('broccoli-es6-module-filter');
+var ES6Modules = require('broccoli-es6modules');
 var mergeTrees = require('broccoli-merge-trees');
 var moveFile = require('broccoli-file-mover');
 var pickFiles = require('broccoli-static-compiler');
@@ -11,8 +11,7 @@ var recast = require('broccoli-es3-safe-recast');
 
 var trees = [
   createAMDTree(),
-  createCommonJSTree(),
-  createStandaloneTree(),
+  createUMDTree(),
 
   // TODO only add tests when Broccoli environment is development ...
   makeTests()
@@ -33,10 +32,12 @@ function makeTests() {
   var jshintTests = jshint('test/tests');
 
   // Create AMD module 'tests' containing all tests in 'test/tests' and concatenate them into tests/tests.js
-  var tests = filterES6Modules('test/tests', {
-    moduleType: 'amd',
-    packageName: 'tests',
-    anonymous: false
+  var tests = new ES6Modules('test', {
+    inputFiles: ["**/*_test.js"],
+    esperantoOptions: {
+      absolutePaths: true,
+      strict: true
+    }
   });
 
   tests = mergeTrees([jshintTests, jshintLib, tests]);
@@ -72,74 +73,35 @@ function makeTests() {
   return mergeTrees([deps, qunit, loader, tests, testsMain]);
 }
 
-
-
 function createAMDTree() {
-  // dist/router.amd.js: all AMD compiled modules concatenated into 1 file
-  var amd = filterES6Modules('lib', {
-    moduleType: 'amd',
-    anonymous: false
+  var amd = new ES6Modules('./lib', {
+    esperantoOptions: {
+      absolutePaths: true,
+      strict: true
+    }
   });
 
   amd = concat(amd, {
-    // to be consinstent with old behavior, we include 'router.js' at the end
-    inputFiles: ['router/**/*.js', 'router.js'],
+    inputFiles: ['**/*.js'],
     outputFile: '/router.amd.js'
   });
 
   return amd;
 }
 
-
-
-function createCommonJSTree() {
-  // CommonJS version of router.js; will be located in 'dist/commonjs'
-  var commonJs = pickFiles('lib', {
-    srcDir: '/',
-    destDir: '/commonjs'
-  });
-  commonJs = filterES6Modules(commonJs, {
-    moduleType: 'cjs'
-  });
-
-  // rename router.js to main.js
-  commonJs = moveFile(commonJs, {
-    srcFile: '/commonjs/router.js',
-    destFile: '/commonjs/main.js'
+function createUMDTree() {
+  var umd = new ES6Modules('./lib', {
+    format: 'umd',
+    bundleOptions: {
+      entry: 'router.js',
+      name: 'router'
+    },
+    esperantoOptions: {
+      strict: true
+    }
   });
 
-  return commonJs;
-}
-
-
-
-function createStandaloneTree() {
-  // dist/router.js: IIFE version of router.js, using RSVP and RouteRecognizer globals
-  var begin = '(function(globals, RSVP, RouteRecognizer) {\n';
-  var end = [];
-  end.push('define("route-recognizer", [], function() { return {"default": RouteRecognizer}; });');
-  end.push('define("rsvp", [], function() { return RSVP;});');
-  end.push("window.Router = requireModule('router');");
-  end.push('}(window, window.RSVP, window.RouteRecognizer));');
-  end = end.join('\n');
-
-  var browser = pickFiles('vendor', {
-    files: ['loader.js'],
-    srcDir: '/',
-    destDir: '/'
-  });
-  browser = mergeTrees([browser, createAMDTree()]);
-  browser = concat(browser, {
-    inputFiles: ['loader.js', '*.js'],
-    outputFile: '/router.js'
-  });
-  browser = wrapFiles(browser, {
-    wrapper: [begin, end],
-    extensions: ['js']
-  });
-
-  // dist/router.min.js
-  var minified = pickFiles(browser, {
+  var minified = pickFiles(umd, {
     srcDir: '/',
     destDir: '/'
   });
@@ -151,5 +113,5 @@ function createStandaloneTree() {
     mangle: true
   });
 
-  return mergeTrees([browser, minified]);
+  return mergeTrees([umd, minified]);
 }
