86951a77abd453cfe746a79cfa7a9193743db4df
Robert Jackson <rjackson@linkedin.com>
2017-09-05 12:55:15 -0400

    Ignore `dist/`.

--- dist/commonjs/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var Router = require("./router/router")["default"];
-var Transition = require("./router/transition").Transition;
-
-exports["default"] = Router;
-
-exports.Transition = Transition;
